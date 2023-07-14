a7fa146b497f1c87a487a003abaf215f23b719f9
Afterlogic Support <support@afterlogic.com>
2023-07-14 10:53:56 +0300

    Added Settings.php and removed config.json

---
+++ Settings.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * This code is licensed under AGPLv3 license or Afterlogic Software License
+ * if commercial version of the product was purchased.
+ * For full statements of the licenses see LICENSE-AFTERLOGIC and LICENSE-AGPL3 files.
+ */
+
+namespace Aurora\Modules\MailSignupDirectadmin;
+
+use Aurora\System\SettingsProperty;
+
+/**
+ * @property bool $Disabled
+ * @property string $DirectAdminURL
+ * @property string $AdminUser
+ * @property string $AdminPass
+ * @property int $UserDefaultQuotaMB
+ */
+
+class Settings extends \Aurora\System\Module\Settings
+{
+    protected function initDefaults()
+    {
+        $this->aContainer = [
+            "Disabled" => new SettingsProperty(
+                false,
+                "bool",
+                null,
+                "Setting to true disables the module",
+            ),
+            "DirectAdminURL" => new SettingsProperty(
+                "http://localhost:2222",
+                "string",
+                null,
+                "Defines main URL of DirectAdmin installation",
+            ),
+            "AdminUser" => new SettingsProperty(
+                "",
+                "string",
+                null,
+                "Username of DirectAdmin administrator account",
+            ),
+            "AdminPass" => new SettingsProperty(
+                "",
+                "string",
+                null,
+                "Password of DirectAdmin administrator account",
+            ),
+            "UserDefaultQuotaMB" => new SettingsProperty(
+                20,
+                "int",
+                null,
+                "Default quota of new email accounts created on DirectAdmin",
+            ),
+        ];
+    }
+}
